d9fda5d1308cf6ab3c98f91a81ae6caee6da6c51
Di Chen <31934037+DiChenB00747156@users.noreply.github.com>
2017-10-02 15:43:58 -0300

    first commit

--- task2.py
+++ task2.py
@@ -1,1 +1,14 @@
-print("ok")
+with open(Crime, 'r') as myfile
+crime_type = ["ASSAULT","ROBBERY","THEFT OF VEHICLE","BREAK AND ENTER","THEFT FROM VEHICLE"]
+crimecount = 0
+for line in myfile:
+	wordlist = line.strip()
+	wordlist1 = wordlist.split(',')
+	for work in wordlist1:
+		if word in crime_type:
+			word_index = wordlist1.find(word)
+			print(word,wordlist1[word_index-1]
+)
+
+	
+
