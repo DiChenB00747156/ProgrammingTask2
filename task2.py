7be9e7cc4b53572dd0fd149e6dba8b8db85ef929
Di Chen <31934037+DiChenB00747156@users.noreply.github.com>
2017-10-02 15:57:43 -0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,14 +1,31 @@
 with open(Crime, 'r') as myfile
 crime_type = ["ASSAULT","ROBBERY","THEFT OF VEHICLE","BREAK AND ENTER","THEFT FROM VEHICLE"]
 crimecount = 0
-for line in myfile:
-	wordlist = line.strip()
-	wordlist1 = wordlist.split(',')
-	for work in wordlist1:
-		if word in crime_type:
-			word_index = wordlist1.find(word)
-			print(word,wordlist1[word_index-1]
-)
+type_id = {}#a dict that mapping from crime types to id
+id_count={}#a dict that mapping from id to count
+
+def get_type_id(file):
+	for line in myfile:
+		wordlist = line.strip()
+		wordlist1 = wordlist.split(',')
+		for work in wordlist1:
+			if word in crime_type:
+				type_id[word] = 1 + type_id.get(word,0)
+	return type_id
+
+def get_id_count(file):
+	for value in type_id.values():
+		id_count[value] = 1 + id_count.get(value,0)
+	return id_count
+
+def print_format(dict1,dict2):
+	for key in dict1:
+		a = dict1[k]
+		print(key,dict[key],dict2[a])
+		
+dict1=get_type_id(myfile)
+dict2=get_id_count(myfile)
+print_format(dict1,dict2
 
 	
 
